f048912f8bce8fb74165187a77a58479843604a9
Black-del-del <lpblack@ualr.edu>
2024-12-03 03:38:28 +0000

    changes to images carousel

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('.fade').slick({
       dots: true,
       infinite: true,
-      speed: 200,
+      speed: 500,
       fade: true,
       slide: 'div',
       cssEase: 'linear',
